8b2cbe2ec2cbc680ec1e4e3386f1ca8f37e650d2
ckloskey <you@example.com>
2018-05-17 09:03:53 -0500

    Initial push of Binary Search Tree

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Node
+    {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node()
+        {
+            val = 0;
+            left = null;
+            right = null;
+        }
+
+        public int Val
+        {
+            get
+            {
+                return val;
+            }
+            set
+            {
+                val = value;
+            }
+        }
+        public Node Left
+        {
+            get
+            {
+                return left;
+            }
+            set
+            {
+                left = value;
+            }
+        }
+        public Node Right
+        {
+            get
+            {
+                return right;
+            }
+            set
+            {
+                right = value;
+            }
+        }
+    }
+    class BinarySearchTree
+    {
+        List<string> nodePath;
+
+        public BinarySearchTree()
+        {
+            nodePath = new List<string>();
+        }
+        public Node InsertNode(Node root, int value)
+        {
+            if (root == null)
+            {
+                root = new Node();
+                root.val = value;
+            }else if(value < root.val)
+            {
+                root.left = InsertNode(root.left, value);
+            }
+            else
+            {
+                root.right = InsertNode(root.right, value);
+            }
+            return root;
+        }
+
+        
+        public List<string> Search(Node root, int value)
+        {
+            if (root == null)
+            {
+                nodePath.Add("Value does not exist in Tree");
+            }else
+            {
+                if (value == root.val)
+                {
+                    if (root.val == value)
+                    {
+                        nodePath.Add("Stop");
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else if (value < root.val)
+                {
+                    nodePath.Add("Left");
+                    Search(root.left, value);
+                    
+                }
+                else
+                {
+                    nodePath.Add("Right");
+                    Search(root.right, value);
+                    
+                }
+                
+            }
+            return nodePath;
+        }
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree bst = new BinarySearchTree();
+            Node root = null;
+            List<int> randomList = new List<int>();
+            Random random = new Random();
+
+
+
+            for (int i = 0; i < 100; i++)
+            {
+                randomList.Add(random.Next(100));
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                root = bst.InsertNode(root, randomList[i]);
+            }
+
+           Node newNode = bst.InsertNode(root, 25);
+
+           List <string> nodePath = bst.Search(root, 25);
+            
+           Console.WriteLine(nodePath);
+        }
+    }
+}
